589b16c343f854af1a2876424705fc86fdd4fe12
RaduSTF21 <lagg1756@gmail.com>
2025-04-06 02:11:13 +0300

    Initialized database, created service for students

---
+++ backend/src/main/java/com/example/demo/controller/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByIdNumber(String idNumber);
+}
