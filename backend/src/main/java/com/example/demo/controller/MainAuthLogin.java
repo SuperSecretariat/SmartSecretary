63086a8ca3d4a0610e800d8c43ca224556bfebb8
RaduSTF21 <lagg1756@gmail.com>
2025-04-06 02:13:01 +0300

    Initialized database, created service for students

--- backend/src/main/java/com/example/demo/controller/MainAuthLogin.java
+++ backend/src/main/java/com/example/demo/controller/MainAuthLogin.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 public class MainAuthLogin {
     public static void main(String[] args)
     {
-        // Loc de test pentru AuthLogin
+        // Loc de test pentru AuthLogin//
     }
 
 }
