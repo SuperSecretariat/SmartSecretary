808d791c32947098db574362f8ddecd3307ee490
RoflexKun <razvan.vilcu2004@gmail.com>
2025-04-09 18:36:27 +0300

    cat sa mearga

--- backend/src/main/java/com/example/demo/controller/CustomUserDetailsService.java
+++ backend/src/main/java/com/example/demo/controller/CustomUserDetailsService.java
@@ -1,12 +1,5 @@
+/*
 package com.example.demo.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -15,13 +8,14 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByIdNumber(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         return new org.springframework.security.core.userdetails.User(
-                user.getIdNumber(),
-                user.getPassword(),
+                user.getUsername(),
+                user.getPassword(), // parola deja hashuită
                 new ArrayList<>()
         );
     }
 }
+*/
