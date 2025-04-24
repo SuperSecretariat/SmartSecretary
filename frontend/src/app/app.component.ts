4a086d3eb5ee602bf9a46dab00206551a73f83dd
Chris5332 <iftimecristian1@gmail.com>
2025-04-24 22:34:11 +0300

    added some content for dashboard

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
+//import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
 
@@ -8,7 +8,7 @@ import { HeaderComponent } from "./header/header.component";
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [DashboardComponent, NavBarComponent, FooterComponent, HeaderComponent],
+  imports: [DashboardComponent, FooterComponent, HeaderComponent] //NavBarComponent
 })
 export class AppComponent{
   title : string = 'Smart Secretary';
--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  showFiller = false;
 }
