09d1af9af40b40ab1f59c42bf03567543bc59b4a
Dani2706 <dumitrascu.stefandaniel27@gmail.com>
2025-04-26 17:32:06 +0300

    Added responsive behaviour to the footer and dynamic behaviour to the menu items

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: AppComponent },
+    { path: '**', redirectTo: 'home'}
+];
--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,16 +1,47 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MenuItem } from '../models/menu-item.model';
+import { CommonModule } from '@angular/common';
+import { OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    CommonModule,
+    RouterLink,
+    RouterOutlet
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-  showFiller = false;
+export class HeaderComponent implements OnInit {
+  showFiller = true;
+  private menuItems: MenuItem[] = [];
+
+  ngOnInit() {
+    
+    // Initialize the menu items here or fetch them from a service
+    this.menuItems = [
+      new MenuItem('Home', '/home'),
+      new MenuItem('About', '/about'),
+      new MenuItem('Contact', '/contact'),
+      new MenuItem('Services', '/services'),
+      new MenuItem('Produces', '/products')
+    ]
+  }
+
+  getMenuItems(): MenuItem[] {
+    return this.menuItems;
+  }
+
 }
