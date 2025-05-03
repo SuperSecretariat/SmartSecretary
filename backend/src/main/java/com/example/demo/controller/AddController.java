7d95d734b73800d753d385585f61e5aecac38c1c
RoflexKun <razvan.vilcu2004@gmail.com>
2025-05-03 13:23:24 +0300

    Fixed validation when adding a user onto it's specific table

--- backend/src/main/java/com/example/demo/controller/AddController.java
+++ backend/src/main/java/com/example/demo/controller/AddController.java
@@ -18,9 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
-/*
-TO-DO: verificarea daca nr matricol/ auth key exista
- */
+
+import static com.example.demo.util.AESUtil.encrypt;
+
 @RestController
 @RequestMapping("/api/add")
 public class AddController {
@@ -36,23 +36,40 @@ public class AddController {
 
     @PostMapping("/student")
     public ResponseEntity<?> addStudent(@RequestBody StudentRequest studentRequest){
-        Student newStudent = new Student(studentRequest.getRegistrationNumber(), studentRequest.getEmail());
-        studentRepository.save(newStudent);
-        return ResponseEntity.ok("New student added to the database");
+        if(studentRepository.existsByRegNumber(studentRequest.getRegistrationNumber()))
+        {
+            Student newStudent = new Student(studentRequest.getRegistrationNumber(), studentRequest.getEmail());
+            studentRepository.save(newStudent);
+            return ResponseEntity.ok("New student added to the database");
+        }
+        else
+            return ResponseEntity.status(409).body("Registration number already in use");
+
     }
 
     @PostMapping("/secretary")
     public ResponseEntity<?> addSecretary(@RequestBody SecretaryRequest secretaryRequest){
-        Secretary newSecretary = new Secretary(secretaryRequest.getAuthKey(), secretaryRequest.getEmail());
-        secretaryRepository.save(newSecretary);
-        return ResponseEntity.ok("New secretary added to the database");
+        if(secretaryRepository.existsByAuthKey(secretaryRequest.getAuthKey())){
+            Secretary newSecretary = new Secretary(secretaryRequest.getAuthKey(), secretaryRequest.getEmail());
+            secretaryRepository.save(newSecretary);
+            return ResponseEntity.ok("New secretary added to the database");
+        }
+        else
+            return ResponseEntity.status(409).body("Authentication key already in use");
+
     }
 
     @PostMapping("/admin")
     public ResponseEntity<?> addAdmin(@RequestBody AdminRequest adminRequest){
-        Admin newAdmin = new Admin(adminRequest.getAuthKey(), adminRequest.getEmail());
-        adminRepository.save(newAdmin);
-        return ResponseEntity.ok("New admin added to the database");
+        if(adminRepository.existsByAuthKey(adminRequest.getAuthKey()))
+        {
+            Admin newAdmin = new Admin(adminRequest.getAuthKey(), adminRequest.getEmail());
+            adminRepository.save(newAdmin);
+            return ResponseEntity.ok("New admin added to the database");
+        }
+        else
+            return ResponseEntity.status(409).body("Authentication key already in use");
+
 
     }
 }
