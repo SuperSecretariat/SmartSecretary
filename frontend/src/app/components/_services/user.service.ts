ef567d313c423a3a6194b25b61dcacae777e95ed
NofalJaber <jabernofal2004@gmail.com>
2025-05-05 14:15:49 +0300

    Account Manager profile completion

--- frontend/src/app/components/_services/user.service.ts
+++ frontend/src/app/components/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080/api/test/'; //De modificat dupa ce face Vilcu AUTH_API-ul in backend
+const API_URL = 'http://localhost:8081/api/test/'; //De modificat dupa ce face Vilcu AUTH_API-ul in backend
 
 @Injectable({
   providedIn: 'root',
